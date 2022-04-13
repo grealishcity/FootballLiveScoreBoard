24ebcb387088731a5bf2fceec1e738ce0ddb1911
Dominik Stepuch <dominik.stepuch@avra.pl>
2022-04-13 15:57:20 +0200

    Throw exception when game list is empty

--- src/main/java/com/grealishcity/scoreboard/dao/GameDao.java
+++ src/main/java/com/grealishcity/scoreboard/dao/GameDao.java
@@ -3,6 +3,7 @@ package com.grealishcity.scoreboard.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.grealishcity.scoreboard.exception.EmptyListException;
 import com.grealishcity.scoreboard.exception.ObjectNotFoundException;
 import com.grealishcity.scoreboard.model.Game;
 import com.grealishcity.scoreboard.model.Team;
@@ -20,10 +21,14 @@ public class GameDao {
     }
 
     public void finish(Team homeTeam, Team awayTeam) {
+        if (games.isEmpty()) {
+            throw new EmptyListException("Game list is empty. Can't remove game from empty list.");
+        }
+
         Game gameToFinish = games.stream()
                 .filter(game -> game.getHomeTeam().equals(homeTeam) && game.getAwayTeam().equals(awayTeam))
                 .findAny()
-                .orElseThrow(() -> new ObjectNotFoundException("Game not found"));
+                .orElseThrow(() -> new ObjectNotFoundException("Game not found for given home team: " + homeTeam + " and away team: " + awayTeam));
 
         games.remove(gameToFinish);
     }
