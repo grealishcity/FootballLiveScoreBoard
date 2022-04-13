51af013ba95e3dfb753b55c4f360eaf5c85a8460
Dominik Stepuch <dominik.stepuch@avra.pl>
2022-04-13 15:48:40 +0200

    Add finish method implementation

--- src/main/java/com/grealishcity/scoreboard/dao/GameDao.java
+++ src/main/java/com/grealishcity/scoreboard/dao/GameDao.java
@@ -3,6 +3,7 @@ package com.grealishcity.scoreboard.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.grealishcity.scoreboard.exception.ObjectNotFoundException;
 import com.grealishcity.scoreboard.model.Game;
 import com.grealishcity.scoreboard.model.Team;
 
@@ -16,6 +17,15 @@ public class GameDao {
 
     public void create(Team homeTeam, Team awayTeam) {
         games.add(new Game(homeTeam, awayTeam));
+    }
+
+    public void finish(Team homeTeam, Team awayTeam) {
+        Game gameToFinish = games.stream()
+                .filter(game -> game.getHomeTeam().equals(homeTeam) && game.getAwayTeam().equals(awayTeam))
+                .findAny()
+                .orElseThrow(() -> new ObjectNotFoundException("Game not found"));
+
+        games.remove(gameToFinish);
     }
 
     public List<Game> getGames() {
