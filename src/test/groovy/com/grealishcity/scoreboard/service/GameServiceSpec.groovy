38be12572e10f3f5b1bf5ab68309d583ceaefbe2
Dominik Stepuch <dominik.stepuch@avra.pl>
2022-04-13 12:39:40 +0200

    add game service test class

---
+++ src/test/groovy/com/grealishcity/scoreboard/service/GameServiceSpec.groovy
@@ -0,0 +1,16 @@
+package com.grealishcity.scoreboard.service
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class GameServiceSpec extends Specification {
+
+    def "should create a new game"() {
+        given:
+
+        when:
+
+        then:
+
+    }
+}
