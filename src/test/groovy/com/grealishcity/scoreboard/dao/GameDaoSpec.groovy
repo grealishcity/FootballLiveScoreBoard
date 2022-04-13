60dbfa28c56061a9f2cfeff2b2f56f911120c441
Dominik Stepuch <dominik.stepuch@avra.pl>
2022-04-13 13:06:39 +0200

    Add game dao test class

---
+++ src/test/groovy/com/grealishcity/scoreboard/dao/GameDaoSpec.groovy
@@ -0,0 +1,19 @@
+package com.grealishcity.scoreboard.dao
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class GameDaoSpec extends Specification {
+
+    @Subject
+    def gameDao = new GameDao()
+    
+    def "should add new match to game list"() {
+        given:
+
+        when:
+
+        then:
+
+    }
+}
