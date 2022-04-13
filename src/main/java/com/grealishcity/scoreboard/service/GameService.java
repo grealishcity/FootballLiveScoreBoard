6deefba2797b5c20e9ed8650e0a165677c96573a
Dominik Stepuch <dominik.stepuch@avra.pl>
2022-04-13 14:08:22 +0200

    Add team validator to game service

--- src/main/java/com/grealishcity/scoreboard/service/GameService.java
+++ src/main/java/com/grealishcity/scoreboard/service/GameService.java
@@ -2,13 +2,16 @@ package com.grealishcity.scoreboard.service;
 
 import com.grealishcity.scoreboard.dao.GameDao;
 import com.grealishcity.scoreboard.model.Team;
+import com.grealishcity.scoreboard.validator.TeamValidator;
 
 public class GameService {
 
     private final GameDao gameDao;
+    private final TeamValidator teamValidator;
 
-    public GameService(GameDao gameDao) {
+    public GameService(GameDao gameDao, TeamValidator teamValidator) {
         this.gameDao = gameDao;
+        this.teamValidator = teamValidator;
     }
 
     public void create(String homeTeamName, String awayTeamName) {
