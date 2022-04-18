1b7ec38f2b9ff8201da572ddce8d85e092bc493d
Dominik Stepuch <dominik.stepuch@avra.pl>
2022-04-18 12:31:11 +0200

    Create main and menu classes

---
+++ src/main/java/com/grealishcity/scoreboard/Main.java
@@ -0,0 +1,11 @@
+package com.grealishcity.scoreboard;
+
+import com.grealishcity.scoreboard.utils.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Menu.display();
+    }
+
+}
---
+++ src/main/java/com/grealishcity/scoreboard/utils/Menu.java
@@ -0,0 +1,13 @@
+package com.grealishcity.scoreboard.utils;
+
+public class Menu {
+
+    public static void display() {
+        System.out.println("------------------------ MENU ------------------------");
+        System.out.println("1. Start a game.");
+        System.out.println("2. Finish game.");
+        System.out.println("3. Update score.");
+        System.out.println("4. Get summary of games by total score.");
+        System.out.println("5. Exit.");
+    }
+}
