9c682db1000245079607558b3ee6ef706aee8c63
Dominik Stepuch <dominik.stepuch@avra.pl>
2022-04-18 12:57:54 +0200

    Create menu structure

--- src/main/java/com/grealishcity/scoreboard/Main.java
+++ src/main/java/com/grealishcity/scoreboard/Main.java
@@ -1,11 +1,40 @@
 package com.grealishcity.scoreboard;
 
+import java.util.Scanner;
+
 import com.grealishcity.scoreboard.utils.Menu;
 
 public class Main {
 
     public static void main(String[] args) {
-        Menu.display();
-    }
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
+        while (true) {
+            Menu.display();
+            System.out.print("Choose an option: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+//                    startGame();
+                    System.out.println(choice);
+                    break;
+                case 2:
+//                    finishGame();
+                    System.out.println(choice);
+                    break;
+                case 3:
+//                    updateScore();
+                    System.out.println(choice);
+                    break;
+                case 4:
+//                    getSummaryByTotalScore();
+                    System.out.println(choice);
+                    break;
+                case 5:
+                    System.exit(0);
+            }
+        }
+    }
 }
