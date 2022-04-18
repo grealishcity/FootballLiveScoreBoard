d6bbeba87264ca3276fdf205cf1ea55694f80501
Dominik Stepuch <dominik.stepuch@avra.pl>
2022-04-18 13:45:19 +0200

    Test fix

--- src/test/groovy/com/grealishcity/scoreboard/service/UserServiceSpec.groovy
+++ src/test/groovy/com/grealishcity/scoreboard/service/UserServiceSpec.groovy
@@ -1,16 +1,18 @@
 package com.grealishcity.scoreboard.service
 
 import spock.lang.Specification
+import spock.lang.Subject
 
 class UserServiceSpec extends Specification {
 
-    def scanner = Mock(Scanner)
-
-    def userService = new UserService(scanner)
+    @Subject
+    def userService
 
     def "should return user choice when input is correct"() {
         given:
         def input = "1"
+        def scanner = new Scanner(input)
+        userService = new UserService(scanner)
 
         when:
         def choice = userService.getUserChoice()
